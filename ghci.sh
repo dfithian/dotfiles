6a69e36a2b48fd0cfc030c841f378ee6136ff9be
Dan Fithian <dan@simspace.com>
2016-07-28 11:29:03 -0400

    Add package to ghci

--- ghci.sh
+++ ghci.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-stack ghci --package classy-prelude --package lens --package time --ghci-options -XNoImplicitPrelude --ghci-options -XScopedTypeVariables
+stack ghci \
+  --package classy-prelude \
+  --package lens \
+  --package monad-control \
+  --package time \
+  --ghci-options -XNoImplicitPrelude \
+  --ghci-options -XScopedTypeVariables
