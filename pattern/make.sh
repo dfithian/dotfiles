c6f5edffe6fd8f558a35b914d126714ececdd69f
Dan Fithian <daniel.m.fithian@gmail.com>
2026-08-28 14:26:46 -0400

    Update

--- pattern/make.sh
+++ pattern/make.sh
@@ -2,13 +2,10 @@
 
 cd "$(dirname "$0")"
 
-for file in gitconfig vimrc zshrc psqlrc; do
+for file in gitconfig vimrc zprofile zshrc psqlrc pgenv; do
     rm -f ~/.$file
     ln -s "$(pwd)/$file" ~/.$file
 done
 
 rm -f ~/.claude/settings.json
 ln -s "$(pwd)/claude/settings.json" ~/.claude/settings.json
-
-rm -rf ~/.local/bin/ppsql
-ln -s "$(pwd)/ppsql" ~/.local/bin/ppsql
